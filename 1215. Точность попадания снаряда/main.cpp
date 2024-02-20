76049d2405fe9b6adeea64c601c467ee14108301
Osipov Rafael <rafael.osipov@rt.ru>
2024-02-20 18:06:02 +0300

    TIMUS-1215: AC

--- 1215. Точность попадания снаряда/main.cpp
+++ 1215. Точность попадания снаряда/main.cpp
@@ -33,7 +33,7 @@ bool isInPolygon(int cx, int cy, vector<Point> polygon, int n)
     int vax = cx - polygon[i].x;
     int vay = cy - polygon[i].y;
 
-    int sign = vx * vay - vax * vy;
+    int sign = (vx * vay - vax * vy) > 0 ? 1 : -1; // TODO ?!
 
     for (i = 1; i < n; ++i) {
         vx = polygon[i + 1].x - polygon[i].x;
@@ -42,7 +42,8 @@ bool isInPolygon(int cx, int cy, vector<Point> polygon, int n)
         vax = cx - polygon[i].x;
         vay = cy - polygon[i].y;
 
-        if (sign != (vx * vay - vax * vy)) {
+        int sign2 = (vx * vay - vax * vy) > 0 ? 1 : -1;
+        if (sign != sign2) {
             return false;
         }
     }
@@ -71,13 +72,33 @@ double findMinDistance(int cx, int cy, vector<Point> polygon, int n)
     //          |
     //  *C------*H---------------*D
     //
+    //  или
+    // 
+    // CD это какая-то сторона полигона
+    // т.А это проекция на прямую CD под 90*
+    //
+    //      |
+    //      |
+    //      *D
+    //      ^
+    //      |    
+    //      |  
+    //      |
+    //      |
+    //      *C
+    //      |
+    //      |
+    //      *H-------*A
+    //      |
+    //      |
+    //
     //
 
     double ans = 10e9;
     const auto A = Point{cx, cy};
 
     // пройдемся по всем парам CD и выберем наименьшую дистанцию
-    for (int i = 1; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         const auto C = polygon[i];
         const double CAx = A.x - C.x;
         const double CAy = A.y - C.y;
@@ -86,30 +107,37 @@ double findMinDistance(int cx, int cy, vector<Point> polygon, int n)
         const double CDx = D.x - C.x;
         const double CDy = D.y - C.y;
         const double lCD = vLen(CDx, CDy);
-        double cosA = (C.x * D.x + C.y * D.y) / vLen(CAx, CAy) * lCD;
+        const double lCA = vLen(CAx, CAy);
+
+        double c1 = (CDx * CAx + CDy * CAy);
+        double c2 = (lCA * lCD);
+        double cosA = c1 / c2;
 
         // 1) нашли длинну вектора CH
-        const double lCH = vLen(CAx, CAy) * cosA;
+        const double lCH = vLen(CAx, CAy) * std::abs(cosA);
 
         // 2) найшли точку H
-        double nCDx = C.x / lCD; // нормированный CD
-        double nCDy = C.y / lCD;
+        double nCDx = CDx / lCD; // нормированный CD
+        double nCDy = CDy / lCD;
 
         double CHx = nCDx * lCH;
         double CHy = nCDy * lCH;
 
-        double AHx = CAx - CHx;
-        double AHy = CAy - CHy;
+        double AHx = CHx - CAx;
+        double AHy = CHy - CAy;
         double lAH = vLen(AHx, AHy);
 
         double Hx = AHx + A.x;
         double Hy = AHy + A.y;
+
         double HDx = D.x - Hx;
         double HDy = D.y - Hy;
         double lHD = vLen(HDx, HDy);
 
         // 3) проверили т.H
-        if (lCH + lHD == lCD) {
+        double r1 = lCH + lHD;
+        double r2 = lCD;
+        if (std::abs(r1 - r2) <= 10e-7) {
             // т.H лежит на отрезке CD => это самое короткое расстояние
             ans = min(ans, lAH);
         } else {
@@ -138,6 +166,7 @@ int main()
         ans = 0;
     } else {
         ans = findMinDistance(cx, cy, polygon, n);
+        ans = std::round(ans * 1000.0) / 1000.0;
     }
 
     printf("%.3f\n", ans);
